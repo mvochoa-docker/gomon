89dfd2d38a0c40772b35db54a218b45950e67ae1
Mario Valentin Ochoa Mota <mvochoaa@gmail.com>
2020-01-12 14:03:01 -0600

    Add image 1.1.0-golang1.11-alpine3.8

---
+++ 1.1.0-golang1.11-alpine3.8/docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -e
+
+DIRECTORY=/etc/ssl/ca-certificates
+
+
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir $DIRECTORY
+    if [ ! -d "/usr/local/share/ca-certificates" ]; then
+        cp -R /usr/local/share/ca-certificates/* $DIRECTORY/
+
+        for ext in crt cer pem; do
+            if [ -f $DIRECTORY/*.$ext ]; then
+                cat $DIRECTORY/*.$ext >> /etc/ssl/certs/ca-certificates.crt
+            fi
+        done
+    fi
+fi
+
+exec "$@"
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Docker image of gomon [https://hub.docker.com/repository/docker/mvochoa/gomon](h
 
 ## Supported tags and respective Dockerfile links
 
-- `1.1.0-golang1.11.1`, `latest` [(1.1.0-golang1.11.1/Dockerfile)](1.1.0-golang1.11.1/Dockerfile)
+- `1.1.0-golang1.11-alpine3.8`, `latest` [(1.1.0-golang1.11-alpine3.8/Dockerfile)](1.1.0-golang1.11-alpine3.8/Dockerfile)
+- `1.1.0-golang1.11.1` [(1.1.0-golang1.11.1/Dockerfile)](1.1.0-golang1.11.1/Dockerfile)
